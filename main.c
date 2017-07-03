4d0f94c3ce1923a7deac2f24a22ce32103fcd49b
anayanaze <anayanaze@hotmail.com>
2017-07-03 14:15:44 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,16 +1,38 @@
-//Ana Flávia Yanaze, Fillipe de Freitas
+//Ana Flávia Yanaze, Filipe de Freitas
 //Trabalho de coloração de grafos - Matemática Discreta 2017.1
 //Prof. Ricardo Luders
 
-#include "dsatur.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 #define VERTICES 20
 #define BUFLEN 256
 
-int main(int argc, const char * argv[]) {
-    Lista graph[VERTICES+1]; //vetor de listas
-   	char str[BUFLEN];
+enum boolean {
+    true = 1, false = 0
+};
+typedef  enum boolean  bool;
+
+
+typedef struct vertice {
+	int id; 
+	char cor;
+	int grau_saturacao;
+	int arestas;
+	bool colorido; //variavel booleana para decidir se o vértice está colorido ou nao
+
+} Vertice; 
+
+Vertice vertice[VERTICES+1];
+int maior_grau (int* grau_n);
+
+int main (){
+	int graph[VERTICES+1][VERTICES+1]; //tamanho maximo
+	int i, v, data, j, grau[VERTICES+1], nos_pintados = 0;
+	int n_cromatico = 0; //numero cromatico
+	char straux[BUFLEN];
     FILE* input;
-	int i, data, j, tam_linha[VERTICES+1];
+
 	char aux;
 	input = fopen("entrada.csv","r+");
     
@@ -18,47 +40,145 @@ int main(int argc, const char * argv[]) {
         printf ("Nao foi possivel abrir o arquivo %s!\n", "entrada.csv");
         exit(1);
     }
-    
-  	for (i=1; i <= VERTICES; i++){ //inicializando listas
-    	graph[i].info = 0;
-    	graph[i].next = NULL;
+
+    //inicializa 
+  	for (i = 1; i <= 20; i++){
+  		vertice[i].id = i;
+  		vertice[i].cor = '0'; 
+  		vertice[i].grau_saturacao = 0;
+  		vertice[i].arestas = 0;
+  		vertice[i].colorido = false; 
   	}
 
-//conta numero de numeros de uma linha
+    //conta numero de valores de uma linha
   	for (i = 1; i <= VERTICES; i++){
   		while (aux != '\n'){
   			if (aux == ','){
-  				tam_linha[i]++;
+  				grau[i]++;
   			}
   			aux = getc(input);
   		}
-  		tam_linha[i] = tam_linha[i] + 1; 
+  		aux = getc(input);
   	}
+
+  	for (i = 1; i <= VERTICES; i++){
+  		printf("Grau do no %d: %d\n", i, grau[i]);
+  	}
+
 
   	rewind(input);
 
   	for (i = 1; i <= VERTICES; i++){
-  		for (j = 1; j <= tam_linha[i]; j++){
-  			fscanf(input, "%s, " &str );
-  			data = atoi (str);
-  			inserir (graph, i, data);
-
-
-  			/*Lista* novo;
-  			novo.info = data;
-  			graph.next = novo;*/
-  			//graph[i] = inserir(graph[i], data);
+  		for (j = 1; j <= grau[i]+1; j++){
+  			fscanf(input, "%s, ", straux);
+  			data = atoi (straux);
+  			graph[i][j] = data;
   		}
   	}
 
   	for(i = 1; i<= VERTICES; i++){
-  		Lista v;
-  		for (v = graph; v != NULL; v = v.next) {
-		printf("Valor: %d\t", v.info);
-  		printf("\n");
+  		for (v = 1; v <= grau[i]+1; v++) {
+
+  			if (v == 1){
+  				printf("Linha %d: ", graph[i][v]);
+  			}
+  			else if (v == grau[i]+1)
+  				printf("%d", graph[i][v]);
+  			else
+				printf("%d, ", graph[i][v]);
+  		}
+  		  		printf("\n");
   	}
 
+bool nao_coloriu, repetiu = false;
+char cor_inicial = 'A';
+char cor_pintada;
+
+  	while (nos_pintados < VERTICES){
+
+  		no_pintado = maior_grau(grau); //nó a ser colorido
+  		nao_coloriu = true;
+  		cor_pintada = cor_inicial;
+
+  		while (nao_coloriu){ //colore o nó e atribui grau de saturacao para nos adjacentes
+  			for (j = 1+1; i <= grau[no_pintado]; j++){
+				if (vertice[graph[no_pintado][j]].cor == cor_pintada)
+					repetiu = true;				
+  			}
+
+			if (!(repetiu)){
+				nao_coloriu = false;
+				nos_pintados++;
+				vertice[no_pintado].colorido = true;
+				verce[no_pintado].cor = cor_pintada;
+				for (j = 1+1; i <= grau[no_pintado]+1; j++){
+					vertice[graph[no_pintado][j]].grau_saturacao++; //aumenta grau de saturação dos nós adjacentes 
+  				}
+			}
+			else
+				cor_pintada++;
+  		}
+
+  	}
+
+  	for (i = 1; i < VERTICES+1; i++){
+  		for (j = 2; j < grau[i]; j++){
+  			if (graph[i][j] == i)
+  				continue;
+  			else
+  				vertice[i].arestas++;
+  		}
+  	}
+
+  	fclose(input);
+
+  	FILE * output;
+  	output = fopen ("output.csv", "w+");
+	fprintf(output, "%s | %s | %s | %s", "Numero_Nos", "Numero_Arestas", "Grau_Nos", "Cores");
+	fprintf(output, "%s | %s | %s | %d", "Numero_Nos", "Numero_Arestas", "", 2012);
+    
+    for (i = 1; i < VERTICES+1; i++){
+		fprintf(output, "%d | %d | %d | %c\n", i, vertice[i].arestas, grau[i], vertice[i].cor);
+
+    }
+
+    fclose(fp);
+    
+
+}
 
 
-    return 0;
+
+int maior_grau (int* grau_n){ //funcao define qual nó vai ser colorido
+
+	int i, maior = 0, j = 1, empate_id[VERTICES+1], n_iguais = 0;
+	bool empate = false, achou = false;
+
+	for (i = 1; i < VERTICES+1; i++){
+		if (grau_n[i] > maior){
+			grau_n[i] = maior;
+			achou = true;
+			n_iguais++;
+		}
+
+		if (n_iguais > 1){ //se houve empate no grau, desempata pelo grau de saturacao
+			empate_id[j] = i; //guarda id dos empates
+			j++; //contador de empates
+		}
+
+	}
+
+	if (empate){ //desempata pelo grau de saturação
+		int 
+		for (i = 1; i <= VERTICES; i++){ //desempata pelo grau de saturacao
+			if (empate_id[i] == vertice.id[i]){
+
+			}
+		}
+	}
+
+
+
+
+	return maior; //retorna id do nó a ser colorido
 }
